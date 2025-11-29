2915b3fed666a1e05453b0cd6abfce1ea4745597
juanignaciomorales <juanignaciomorales2015@gmail.com>
2025-11-29 20:54:40 -0300

    actividad unidad 1

---
+++ script.js
@@ -0,0 +1,15 @@
+// Variables
+
+// Number
+let edad = 25;
+
+// String
+let nombre = "Ignacio";
+
+// Boolean
+let esEstudiante = true;
+
+// Mostrar valores en consola
+console.log("Edad:", edad);
+console.log("Nombre:", nombre);
+console.log("¿Es estudiante?:", esEstudiante);
