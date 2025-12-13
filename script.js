cf557f0e8291d8beb725c2b178693987013a3cb3
juanignaciomorales <juanignaciomorales2015@gmail.com>
2025-12-13 20:23:11 -0300

    Modificaciones

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
-// Variables
-
-// Number
 let edad = 25;
-
-// String
 let nombre = "Ignacio";
+let estudiante = true;
+console.log(edad);
+console.log(nombre);
+console.log(estudiante);
+const dni = 42265605;
+console.log(dni);
+nombre = "Flor";
+console.log(nombre);
 
-// Boolean
-let esEstudiante = true;
+let suma1 = 20 ;
+let suma2 = 30 ;
+console.log(suma1+suma2);
 
-// Mostrar valores en consola
-console.log("Edad:", edad);
-console.log("Nombre:", nombre);
-console.log("¿Es estudiante?:", esEstudiante);
+edad = parseInt (prompt("Que edad tenes?"));
+console.log(edad);
+
+let mayor = confirm ("Sos mayor de edad?");
+console.log(mayor);
+
+let mate = prompt ("Tomas mate?");
+console.log(mate);
+
+alert ("Biemvenido a mi sitio")
+
+if (edad>25 && edad<50) {
+    console.log("Condicion Cumplida");}
+  else{
+        console.log("Condicion NO Cumplida")
+};
