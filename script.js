d676e13f37d3d7e59ef071cb2a14dc65855cde8c
juanignaciomorales <juanignaciomorales2015@gmail.com>
2026-02-01 15:18:26 -0300

    modificaciones

--- script.js
+++ script.js
@@ -111,7 +111,104 @@ console.log(usuario);
 sessionStorage.removeItem ("usuario");
  */
 
-const numeros = [1,2,3,4,5];
+/* let usuario = {
+    nombre: "Nacho",
+    edad: "26"
+}
+
+localStorage.setItem("usuario", JSON.stringify(usuario));
+
+let usuarioRecuperado = JSON.parse(localStorage.getItem ("usuario"));
+console.log(usuarioRecuperado); */
+
+/* localStorage.remove; */
+
+/* const numeros = [1,2,3,4,5,6];
+
+/* /* for each */
+/* numeros.forEach(n=>{
+    console.log(n);
+}); */
+
+/* /* map */
+/* const doble = numeros.map(n=>n*2);
+console.log(doble); */ 
+
+/* const mayoresA3 = numeros.filter(n=> n>3);
+console.log(mayoresA3); */
+
+/* const numerosPares = numeros.filter (function(numero){
+    return numero % 2 === 0;
+})
+
+console.log(numerosPares); */
+
+/* const cursos = [
+    { nombre: 'JavaScript', duracion: 8 },
+    { nombre: 'React', duracion: 6 },
+    { nombre: 'Angular', duracion: 10 }
+];
+
+const cursosLargos = cursos.filter (function(cursos) {
+    return cursos.duracion > 7
+});
+console.log(cursosLargos); */
+
+/* const numeros = [1,2,3,4,5,6];
+
+const suma = numeros.reduce ((acumulador,numero)=> {
+    return acumulador + numero;
+},0);
+console.log(suma);
+ */
+
+/* const precios = [100,250,50];
+const total = precios.reduce ((acc,precio) => acc + precio,0);
+console.log(total); */
+
+/* const numeros = [1,2,3,4,5]; */
+
+/* const numeroEncontrado = numeros.find (n=> n>3);
+console.log(numeroEncontrado); */
+
+/* const hayMayorA4 = numeros.some (n => n>4);
+console.log(hayMayorA4);
+ */
+
+/* let div = document.getElementById ("div");
+let parrafo = document.getElementById ("p");
+
+console.log(div.innerHTML);
+console.log(p.innerHTML);
+
+let paises = document.getElementsByClassName ("paises")
+console.log(paises [0].innerHTML);
+console.log(paises [1].innerHTML);
+console.log(paises [2].innerHTML);
+
+let contenedores = document.getElementsByTagName ("div");
+console.log(contenedores [0].innerHTML);
+console.log(contenedores [1].innerHTML); */
+
+let h1 = document.querySelectorAll ("h1")
+console.log(h1);
+
+let paises = document.querySelectorAll (".paises")
+console.log(paises);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
